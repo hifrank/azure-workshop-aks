3930209549475ebe3b36d4d1ef8a33718a9966bb
Frank Chen <hifrankc@gmail.com>
2022-12-09 07:57:51 +0800

    update

--- provision-aks/terraform/private-aks/main.tf
+++ provision-aks/terraform/private-aks/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-# 1. provision (4-10m)
+# 1. provision AKS cluster(4-10m)
 ###############################################
 ###############################################
 module "aks_cluster" {
   source                       = "./modules/aks-private-cluster"
-  enable_private_cluster       = false
+  enable_private_cluster       = true
   rg_name                      = var.aks_rg_name
   location                     = var.location
   cluster_name                 = var.cluster_name
@@ -17,7 +17,7 @@ module "aks_cluster" {
   default_agent_pool_subnet_id = var.default_agent_pool_subnet_id
 }
 
-# image registry
+# 1.2 image registry
 ###############################################
 ## habor setting
 ## ACR
@@ -27,52 +27,52 @@ module "aks_cluster" {
 ###############################################
 # 2.1 node pool
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster_node_pool
-resource "azurerm_kubernetes_cluster_node_pool" "regular" {
-  name                  = "regular"
-  vnet_subnet_id = var.default_agent_pool_subnet_id
-  kubernetes_cluster_id = module.aks_cluster.cluster_id
-  vm_size               = "Standard_B2s"
-  enable_auto_scaling   = true
-  max_count             = 5
-  min_count             = 1
+# resource "azurerm_kubernetes_cluster_node_pool" "regular" {
+#   name                  = "regular"
+#   vnet_subnet_id = var.default_agent_pool_subnet_id
+#   kubernetes_cluster_id = module.aks_cluster.cluster_id
+#   vm_size               = "Standard_B2s"
+#   enable_auto_scaling   = true
+#   max_count             = 5
+#   min_count             = 1
 
-  tags = {
-    Environment = "Production"
-    Node        = "Regular"
-  }
-}
+#   tags = {
+#     Environment = "Production"
+#     Node        = "Regular"
+#   }
+# }
 # 2.2 spot node pool
 # https://learn.microsoft.com/en-us/azure/aks/spot-node-pool
-resource "azurerm_kubernetes_cluster_node_pool" "spot" {
-  name                  = "spot"
-  vnet_subnet_id = var.default_agent_pool_subnet_id
-  kubernetes_cluster_id = module.aks_cluster.cluster_id
-  # https://aka.ms/azureskunotavailable
-  vm_size               = "Standard_D2_v5"
-  enable_auto_scaling   = true
-  max_count             = 5
-  min_count             = 1
-  priority              = "Spot"
-  eviction_policy       = "Delete"
-  node_taints           = [
-    "spot:NoSchedule",
-    "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
-    ]
-  node_labels = {
-    "kubernetes.azure.com/scalesetpriority": "spot",
-    "name" : "spot"
-  }
-  tags = {
-    Environment = "Production"
-    Node        = "Spot"
-  }
-}
+# resource "azurerm_kubernetes_cluster_node_pool" "spot" {
+#   name                  = "spot"
+#   vnet_subnet_id = var.default_agent_pool_subnet_id
+#   kubernetes_cluster_id = module.aks_cluster.cluster_id
+#   # https://aka.ms/azureskunotavailable
+#   vm_size               = "Standard_D2_v5"
+#   enable_auto_scaling   = true
+#   max_count             = 5
+#   min_count             = 1
+#   priority              = "Spot"
+#   eviction_policy       = "Delete"
+#   node_taints           = [
+#     "spot:NoSchedule",
+#     "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
+#     ]
+#   node_labels = {
+#     "kubernetes.azure.com/scalesetpriority": "spot",
+#   }
+#   tags = {
+#     Environment = "Production"
+#     Node        = "Spot"
+#   }
+# }
 
 # 3. services
 ###############################################
 # 3.1 ingress
 # nginx
 # agic
+# https://learn.microsoft.com/en-us/azure/developer/terraform/create-k8s-cluster-with-aks-applicationgateway-ingress
 
 # 3.2 storage class
 # application package
